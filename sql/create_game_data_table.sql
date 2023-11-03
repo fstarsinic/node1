d048ed8615d42a8fe7f9f581e78e11d6b7bc9fc4
fstarsinic@drfirst.com <fstarsinic@drfirst.com>
2023-11-03 08:26:46 -0700

    cleanup

---
+++ sql/create_game_data_table.sql
@@ -0,0 +1,21 @@
+CREATE TABLE game_data (
+    GameId INTEGER,
+    Team TEXT,
+    Opponent TEXT,
+    Player TEXT,
+    Game TEXT,
+    Points INTEGER,
+    ORebounds INTEGER,
+    DRebounds INTEGER,
+    Assists INTEGER,
+    Steals INTEGER,
+    Blocks INTEGER,
+    Turnovers INTEGER,
+    TwoPointAttempted INTEGER,
+    TwoPointMade INTEGER,
+    ThreePointAttempted INTEGER,
+    ThreePointMade INTEGER,
+    FTAttempted INTEGER,
+    FTMade INTEGER
+);
+
