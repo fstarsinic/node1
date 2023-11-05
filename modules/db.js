2691fce0cfce503a3a615b299f194fff5f250476
fstarsinic@drfirst.com <fstarsinic@drfirst.com>
2023-11-05 11:19:41 -0800

    work on packed bubble visualizaiton

--- modules/db.js
+++ modules/db.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database('mydatabase.db');
 // Function to fetch data from the database based on the query parameter
 function get_top_ppg(num, callback) {
   console.log('db.get_top_ppg()')
-  const query = `SELECT Player, Points FROM bcb_stats order by Points desc limit ${num}`;
+  const query = `SELECT Player, avg(Points) FROM game_data group by Player order by avg(Points) desc limit ${num}`;
   db.all(query, (err, rows) => {
     if (err) {
       callback(err, null);
@@ -19,7 +19,7 @@ function get_top_ppg(num, callback) {
 // Function to fetch data from the database based on the query parameter
 function get_top_turnovers(num, callback) {
     console.log('db.get_top_turnovers()')
-    const query = `SELECT Player, Turnovers FROM bcb_stats order by Turnovers desc limit ${num}`;
+    const query = `SELECT Player, avg(Turnovers) FROM game_data  group by Player order by avg(Turnovers) desc limit ${num}`;
     db.all(query, (err, rows) => {
       if (err) {
         callback(err, null);
@@ -32,7 +32,7 @@ function get_top_turnovers(num, callback) {
  // Function to fetch data from the database based on the query parameter
 function get_top_rebounds(num, callback) {
     console.log('db.get_top_rebounds()')
-    const query = `SELECT Player, Rebounds FROM bcb_stats order by Rebounds desc limit ${num}`;
+    const query = `SELECT Player, avg(Rebounds) FROM game_data  group by Player order by avg(Rebounds) desc limit ${num}`;
     db.all(query, (err, rows) => {
       if (err) {
         callback(err, null);
@@ -45,7 +45,7 @@ function get_top_rebounds(num, callback) {
  // Function to fetch data from the database based on the query parameter
  function get_top_multi(num, callback) {
   console.log('db.get_top_multi()')
-  const query = `SELECT Player, Rebounds, Points, Turnovers FROM bcb_stats order by Rebounds desc limit ${num}`;
+  const query = `SELECT Player, sum(Rebounds), sum(Points), sum(Turnovers) FROM game_data  group by Player order by Player limit ${num}`;
   db.all(query, (err, rows) => {
     if (err) {
       console.log(`error: ${err}`)
@@ -60,7 +60,7 @@ function get_top_rebounds(num, callback) {
  // Function to fetch data from the database based on the query parameter
  function get_multi(callback) {
   console.log('db.get_multi()')
-  const query = `SELECT Player, Rebounds, Points, Turnovers FROM bcb_stats order by Player`;
+  const query = `SELECT Player, sum(Rebounds), sum(Points), sum(Turnovers) FROM game_data  group by Player order by Player`;
   db.all(query, (err, rows) => {
     if (err) {
       console.log(`error: ${err}`)
@@ -75,8 +75,12 @@ function get_top_rebounds(num, callback) {
  // Function to fetch data from the database based on the query parameter
  function get_player_data(callback) {
   console.log('db.get_player_data()')
-  const query = `SELECT ID,Player,Points,Rebounds,Assists,Steals,Blocks,Turnovers,FieldGoalPct,ThreePointPercent,FTPercent,TrueShootingPct
-  FROM bcb_stats order by Player`;
+  const query = `SELECT p.firstname, p.lastname, t.team_name, avg(points), avg(g.ORebounds+g.DRebounds) as Rebounds,avg(g.Assists),avg(g.Steals),avg(g.Blocks),avg(g.Turnovers),
+  round(sum(g.TwoPointMade) *100 / sum(g.TwoPointattempted),2) as FieldGoalPct,
+  round(sum(g.ThreePointMade) * 100 / sum(g.ThreePointAttempted),2) as ThreePointPercent,
+  round(sum(g.FTMade) *100 /sum(g.FTAttempted),2) as FTPercent 
+  FROM game_data g, player p, team t where g.player_id = p.id and g.team_id = t.team_id group by Player order by Player`;
+  console.log(query)
   db.all(query, (err, rows) => {
     if (err) {
       console.log(`error: ${err}`)
@@ -94,6 +98,23 @@ function get_top_rebounds(num, callback) {
   });
 }
 
+ // Function to fetch data from the database based on the query parameter
+ function get_team_game_data(num, callback) {
+  console.log('db.get_team_game_data()')
+  const query = `select Player, Game, Opponent, Points, player_id,team_id from game_data where team_id = ${num} order by player, game;`;
+  console.log(query)
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.log(`error: ${err}`)
+      callback(err, null);
+    } else {
+      console.log('Success getting data')
+      console.log(rows)  
+      callback(null, rows)
+    }
+  });
+}
+
 module.exports = {
-    get_top_ppg, get_top_turnovers, get_top_rebounds, get_top_multi, get_multi, get_player_data,
+    get_top_ppg, get_top_turnovers, get_top_rebounds, get_top_multi, get_multi, get_player_data, get_team_game_data
 };
---
+++ public/js/player_packed_bubble.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch the integer value from the query parameter "number"
+    const queryParams = new URLSearchParams(window.location.search);
+    var num = 0
+    if (queryParams.has('team')) {
+        num = parseInt(queryParams.get('team'));
+        console.log(`num is ${num}`)
+    } else{
+        num = 0;
+    }
+    console.log(`result: num is ${num}`)
+  
+      // Fetch data from server
+      fetch(`/api/teamgames?num=${num}`)
+        .then((response) => response.json())
+        .then((data) => {
+          // Extract player names and points from data
+          //const players = data.map((item) => item.Player);
+          //const points = data.map((item) => item.Points);
+    
+            // Create the Highcharts hbar chart
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'packedbubble',
+                    height: '100%'
+                },
+                title: {
+                    text: `Team Data`,
+                    align: 'left'
+                },
+                tooltip: {
+                    useHTML: true,
+                    pointFormat: '<b>{point.name}:</b> {point.value}points'
+                },
+                plotOptions: {
+                    packedbubble: {
+                        minSize: '20%',
+                        maxSize: '100%',
+                        zMin: 0,
+                        zMax: 1000,
+                        layoutAlgorithm: {
+                            gravitationalConstant: 0.05,
+                            splitSeries: true,
+                            seriesInteraction: false,
+                            dragBetweenSeries: true,
+                            parentNodeLimit: true
+                        },
+                        dataLabels: {
+                            enabled: true,
+                            format: '{point.name} - {point.value}',
+                            filter: {
+                                property: 'y',
+                                operator: '>',
+                                value: -1
+                            },
+                            style: {
+                                color: 'black',
+                                textOutline: 'none',
+                                fontWeight: 'normal'
+                            }
+                        }
+                    }
+                },
+                series: data
+            });
+        });
+    });
